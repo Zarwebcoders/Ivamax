4f82fd338f778240163d1b7af2a6af4bad5aa586
b3ingsahil007 <chhipasahil163@gmail.com>
2026-02-13 19:11:50 +0530

    CORS Error Again

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ initCronJobs();
 const allowedOrigins = [
     'http://localhost:5173',
     'https://ivamax-frontend.vercel.app',
-    'http://192.168.1.3:5173'
+    'http://192.168.1.3:5173',
+    'https://ivamax.vercel.app'
 ];
 
 app.use(cors({
