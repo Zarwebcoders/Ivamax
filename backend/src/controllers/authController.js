dfa2917c8a74e7047f8dbe28128606ca6c7f234e
b3ingsahil007 <chhipasahil163@gmail.com>
2026-02-16 14:35:26 +0530

    Login Updated

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -352,9 +352,9 @@ const login = async (req, res) => {
         const user = await User.findOne(query);
 
         if (user && (await user.comparePassword(password))) {
-            if (!user.isActive) {
-                return res.status(403).json({ message: 'Account is inactive. Please contact support.' });
-            }
+            // if (!user.isActive) {
+            //     return res.status(403).json({ message: 'Account is inactive. Please contact support.' });
+            // }
 
             const treeNode = await Tree.findOne({ userId: user.userId });
             res.json({
